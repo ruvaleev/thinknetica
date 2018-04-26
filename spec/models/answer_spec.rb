b59608422eab529cbfd853d01501876d7bd51ac5
Ruslan Valeev <ruvaleev@gmail.com>
2018-04-26 14:26:09 +0300

    dz7 edition 4

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   let(:question) { create(:question) }
   let(:answer) { create(:answer, question: question) }
-  let(:former_best_answer) { create(:answer, question: question) }
+  let!(:former_best_answer) { create(:answer, question: question, award: true) }
   
   it { should belong_to :question }
   it { should validate_presence_of :body }
@@ -14,9 +14,8 @@ RSpec.describe Answer, type: :model do
   end
 
   it 'former best answer is not the best now' do
-    former_best_answer.make_best
     answer.make_best
     former_best_answer.reload
-    expect( former_best_answer.award ).to eq(0)
+    expect( former_best_answer ).to_not be_award
   end
 end
