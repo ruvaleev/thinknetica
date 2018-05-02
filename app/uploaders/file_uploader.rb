0f959af9041488cbde8af4e091760e051bf8dbe0
Ruslan Valeev <ruvaleev@gmail.com>
2018-05-02 14:35:30 +0300

    Added link to file

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,4 +1,6 @@
 class FileUploader < CarrierWave::Uploader::Base
+  delegate :identifier, to: :file
+
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
