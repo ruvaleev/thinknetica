03898ebbb11e8137a1113b0cbdae359eef358bb3
Ruslan Valeev <ruvaleev@gmail.com>
2018-05-29 10:53:26 +0300

    final refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def vote_url(resource, value)
-    polymorphic_path( [ resource ], 
+    polymorphic_path( resource, 
                       action: 'create_vote', 
                       id: resource, 
                       value: value, 
