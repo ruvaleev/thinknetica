2da831823a7d52e005f4e927479ae45b8a40ec0c
Ruslan Valeev <ruvaleev@gmail.com>
2018-06-10 12:11:51 +0300

    done1

--- app/channels/answers_channel.rb
+++ app/channels/answers_channel.rb
@@ -1,7 +1,5 @@
 class AnswersChannel < ApplicationCable::Channel
   def follow(data)
-    logger.debug "в канал пришло #{data}"
-    logger.debug "data question id в канал пришло #{data['question_id']}"
     stream_from "answers_#{data['question_id']}"
   end
 end
